06f71b8b6e4b82ebb95d522c29419a14fa8e047a
qincong_hu <82565126@qq.com>
2020-07-11 18:30:03 +0800

    第一次

---
+++ src/exer.java
@@ -0,0 +1,14 @@
+/**
+ * @Description:
+ * @ClassName: exer
+ * @Author: phils
+ * @Date: 2020/7/11 17:37
+ * @Version: 1.0
+ */
+public class exer {
+
+    public static void main(String[] args) {
+
+        System.out.println("测试");
+    }
+}
